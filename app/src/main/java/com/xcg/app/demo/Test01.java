baa6f5372bf5a111863cca5ff5f881cab2c8f02d
xcg <afterxiong@sina.cn>
2018-06-22 15:15:45 +0800

    sync1

--- app/src/main/java/com/xcg/app/demo/Test01.java
+++ app/src/main/java/com/xcg/app/demo/Test01.java
@@ -1,8 +1,13 @@
 package com.xcg.app.demo;
 
+import com.socks.library.KLog;
+
 /**
  * Created by xiongchengguang on 2018/6/22.
  */
 
 public class Test01 {
+    public static void main(String[] args) {
+        KLog.d("xiong chengguag ");
+    }
 }
